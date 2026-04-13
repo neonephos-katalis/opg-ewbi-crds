8b573c43a323b376c91e4d14ba253257f1493d39
Alvaro Fernandez <alvaro.fernandezgarrido.ext@telefonica.com>
2026-04-13 15:01:35 +0200

    Small fixes in avaiabilityzone_types.go
    Added descriptions, modified shortname and changed int32 to int in some
    fields.

--- opg-ewbi-crds/api/v1beta1/avaiabilityzone_types.go
+++ opg-ewbi-crds/api/v1beta1/avaiabilityzone_types.go
@@ -21,56 +21,71 @@ import (
 )
 
 type GPUResource struct {
+	// GPU vendor name e.g. NVIDIA, AMD etc.
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Enum=GPU_PROVIDER_NVIDIA;GPU_PROVIDER_AMD
+	// +kubebuilder:example=Nvidia
 	GpuVendorType string `json:"gpuVendorType"`
 
+	// Model name corresponding to vendorType may include info e.g. for NVIDIA, model name could be “Tesla M60”, “Tesla V100” etc.
 	// +kubebuilder:validation:Required
 	GpuModeName string `json:"gpuModeName"`
 
+	// GPU memory in Mbytes
 	// +kubebuilder:validation:Required
-	GpuMemory int32 `json:"gpuMemory"`
+	GpuMemory int `json:"gpuMemory"`
 
+	// Number of GPUs
 	// +kubebuilder:validation:Required
-	NumGPU int32 `json:"numGPU"`
+	NumGPU int `json:"numGPU"`
 }
 
 type Hugepage struct {
+	// Size of hugepage
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Enum="2MB";"4MB";"1GB"
 	PageSize string `json:"pageSize"`
 
+	// Total number of huge pages
 	// +kubebuilder:validation:Required
-	Number int32 `json:"number"`
+	Number int `json:"number"`
 }
 
 type ComputeResourceInfo struct {
+	// CPU Instruction Set Architecture (ISA) E.g., Intel, Arm etc.
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Enum=ISA_X86_64;ISA_ARM_64
 	CpuArchType string `json:"cpuArchType"`
 
+	// Number of vcpus in whole, decimal up to millivcpu, or millivcpu format.
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Pattern=`^\d+((\.\d{1,3})|(m))?$`
+	// +kubebuilder:example={ "whole": { "value": "2" }, "decimal": { "value": "0.500" }, "millivcpu": { "value": "500m" } }
 	NumCPU string `json:"numCPU"`
 
+	// Amount of RAM in Mbytes
 	// +kubebuilder:validation:Required
 	Memory int64 `json:"memory"`
 
+	// Amount of disk storage in Gbytes for a given ISA type
 	// +kubebuilder:validation:Optional
 	DiskStorage int32 `json:"diskStorage,omitempty"`
 
 	// +kubebuilder:validation:Optional
 	Gpu []GPUResource `json:"gpu,omitempty"`
 
+	// Number of Intel VPUs available for a given ISA type
 	// +kubebuilder:validation:Optional
-	Vpu int32 `json:"vpu,omitempty"`
+	Vpu int `json:"vpu,omitempty"`
 
+	// Number of FPGAs available for a given ISA type
 	// +kubebuilder:validation:Optional
-	Fpga int32 `json:"fpga,omitempty"`
+	Fpga int `json:"fpga,omitempty"`
 
 	// +kubebuilder:validation:Optional
 	Hugepages []Hugepage `json:"hugepages,omitempty"`
 
+	// Support for exclusive CPUs
 	// +kubebuilder:validation:Optional
 	CpuExclusivity bool `json:"cpuExclusivity,omitempty"`
 }
@@ -78,6 +93,7 @@ type ComputeResourceInfo struct {
 type SupportedOSType struct {
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Enum=x86_64;x86
+	// +kubebuilder:example=x86_64
 	Architecture string `json:"architecture"`
 
 	// +kubebuilder:validation:Required
@@ -94,61 +110,76 @@ type SupportedOSType struct {
 }
 
 type FlavourSupported struct {
+	// An identifier to refer to a specific combination of compute resources
 	// +kubebuilder:validation:Required
 	FlavourId string `json:"flavourId"`
 
+	// CPU Instruction Set Architecture (ISA) E.g., Intel, Arm etc.
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Enum=ISA_X86;ISA_X86_64;ISA_ARM_64
 	CpuArchType string `json:"cpuArchType"`
 
+	// A list of operating systems which this flavour configuration can support e.g., RHEL Linux, Ubuntu 18.04 LTS, MS Windows 2012 R2.
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:MinItems=1
 	SupportedOSTypes []SupportedOSType `json:"supportedOSTypes"`
 
+	// Number of available vCPUs
 	// +kubebuilder:validation:Required
 	NumCPU int32 `json:"numCPU"`
 
+	// Amount of RAM in Mbytes
 	// +kubebuilder:validation:Required
 	MemorySize int32 `json:"memorySize"`
 
+	// Amount of disk storage in Gbytes
 	// +kubebuilder:validation:Required
 	StorageSize int32 `json:"storageSize"`
 
 	// +kubebuilder:validation:Optional
 	Gpu []GPUResource `json:"gpu,omitempty"`
 
+	// Number of FPGAs
 	// +kubebuilder:validation:Optional
 	Fpga int32 `json:"fpga,omitempty"`
 
+	// Number of Intel VPUs available
 	// +kubebuilder:validation:Optional
-	Vpu int32 `json:"vpu,omitempty"`
+	Vpu int `json:"vpu,omitempty"`
 
 	// +kubebuilder:validation:Optional
 	Hugepages []Hugepage `json:"hugepages,omitempty"`
 
+	// Support for exclusive CPUs
 	// +kubebuilder:validation:Optional
 	CpuExclusivity bool `json:"cpuExclusivity,omitempty"`
 }
 
 type NetworkResources struct {
+	// Max dl throughput that this edge can offer. It is defined in Mbps.
 	// +kubebuilder:validation:Required
 	EgressBandWidth int32 `json:"egressBandWidth"`
 
+	// Number of network interface cards which can be dedicatedly assigned to application pods on isolated networks. This includes virtual as well physical NICs
 	// +kubebuilder:validation:Required
 	DedicatedNIC int32 `json:"dedicatedNIC"`
 
+	// If this zone support SRIOV networks or not
 	// +kubebuilder:validation:Required
 	SupportSriov bool `json:"supportSriov"`
 
+	// If this zone supports DPDK based networking.
 	// +kubebuilder:validation:Required
 	SupportDPDK bool `json:"supportDPDK"`
 }
 
 type LatencyRanges struct {
+	// The time for data/packet to reach from UC to edge application. It represent mínimum latency in milli seconds that may exist between UCs and edge apps in this zone but it can be higher in actual.
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:validation:Minimum=1
 	MinLatency int32 `json:"minLatency,omitempty"`
 
+	// The maximum limit of latency between UC and Edge App in milli seconds.
 	// +kubebuilder:validation:Optional
 	MaxLatency int32 `json:"maxLatency,omitempty"`
 }
@@ -158,15 +189,18 @@ type JitterRanges struct {
 	// +kubebuilder:validation:Minimum=1
 	MinJitter int32 `json:"minJitter,omitempty"`
 
+	// The maximum limit of network jitter between UC and Edge App in milli seconds.
 	// +kubebuilder:validation:Optional
 	MaxJitter int32 `json:"maxJitter,omitempty"`
 }
 
 type ThroughputRanges struct {
+	// The minimum limit of network throughput between UC and Edge App in Mega bits per seconds (Mbps).
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:validation:Minimum=1
 	MinThroughput int32 `json:"minThroughput,omitempty"`
 
+	// The maximum limit of network throughput between UC and Edge App in Mega bits per seconds (Mbps).
 	// +kubebuilder:validation:Optional
 	MaxThroughput int32 `json:"maxThroughput,omitempty"`
 }
@@ -184,10 +218,12 @@ type ZoneServiceLevelObjsInfo struct {
 
 // AvaiabilityZoneSpec defines the desired state of AvaiabilityZone
 type AvaiabilityZoneSpec struct {
+	// This identifier shall be provided by the partner OP on successful verification and validation of the federation create request and is used by partner op to identify this newly created federation context. Originating OP shall provide this identifier in any subsequent request towards the partner op.
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Pattern=`^[A-Za-z0-9][A-Za-z0-9-]*$`
 	FederationContextId string `json:"federationContextId"`
 
+	// Human readable name of the zone.
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:validation:Pattern=`^[A-Za-z0-9][A-Za-z0-9-]*$`
 	ZoneId string `json:"zoneId,omitempty"`
@@ -198,10 +234,12 @@ type AvaiabilityZoneSpec struct {
 
 // AvaiabilityZoneStatus defines the observed state of AvaiabilityZone.
 type AvaiabilityZoneStatus struct {
+	// Resources exclusively reserved for the originator OP.
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:validation:MinItems=1
 	ReservedComputeResources []ComputeResourceInfo `json:"reservedComputeResources,omitempty"`
 
+	// Max quota on resources partner OP allows over reserved resources.
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:validation:MinItems=1
 	ComputeResourceQuotaLimits []ComputeResourceInfo `json:"computeResourceQuotaLimits,omitempty"`
@@ -213,13 +251,14 @@ type AvaiabilityZoneStatus struct {
 	// +kubebuilder:validation:Optional
 	NetworkResources *NetworkResources `json:"networkResources,omitempty"`
 
+	// It is a measure of the actual amount of data that is being sent over a network per unit of time and indicates máximum supported value for a zone
 	// +kubebuilder:validation:Optional
 	ZoneServiceLevelObjsInfo *ZoneServiceLevelObjsInfo `json:"zoneServiceLevelObjsInfo,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
-// +kubebuilder:resource:shortName=zone,scope=Namespaced
+// +kubebuilder:resource:shortName=avazone,scope=Namespaced
 // +kubebuilder:printcolumn:name="FederationContextId",type=string,JSONPath=`.spec.federationContextId`
 
 // AvaiabilityZone is the Schema for the avaiabilityzones API
